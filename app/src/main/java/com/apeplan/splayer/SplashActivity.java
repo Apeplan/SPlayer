078845e2b9d604bb01f10d6509b4154c940a113d
Simon Han <hanzx1024@gmail.com>
2017-03-08 22:30:41 +0800

    update

--- app/src/main/java/com/apeplan/splayer/SplashActivity.java
+++ app/src/main/java/com/apeplan/splayer/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.apeplan.splayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.File;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,4 +13,31 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
     }
+
+    private class ScanVideoTask extends AsyncTask<Void, File, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
+
+        public void eachVideo(File file) {
+            if (file != null && file.exists() && file.isDirectory()) {
+                File[] files = file.listFiles();
+                if (files != null && files.length != 0) {
+                    for (File f : files) {
+                        if (f.isDirectory()) {
+                            eachVideo(f);
+                        } else if (f.exists() && f.canRead()){
+
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
 }
